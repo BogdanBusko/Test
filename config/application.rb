8bf50dae894a6ac0ce1f30b996fe7320144c4c75
BuskoBogdan <busko.bogdan@gmail.com>
2017-01-11 16:59:22 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,6 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-Mongoid.load!('config/mongoid.yml')
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
